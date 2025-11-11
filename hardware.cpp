3ca783e5211d1968a859e5b31f00953e390fe197
Colin <62516283+faletto@users.noreply.github.com>
2025-11-10 18:31:45 -0800

    Add sensor initialization methods

--- hardware.cpp
+++ hardware.cpp
@@ -1,6 +1,32 @@
 #include "hardware.h"
 
-int HardwareInit() {
-    //TODO
-    return 0;
+bool HardwareInit() {
+    return ImuInit() && BaroInit() && GPSInit();
+}
+
+// Initializes the IMU
+bool ImuInit() {
+    Wire.begin();
+    if (imu.begin() != ICM_20948_Stat_Ok) return false;
+    imu.setAccelRange(ICM_20948_ACCEL_RANGE_4G);
+    imu.setGyroRange(ICM_20948_GYRO_RANGE_500DPS);
+    return true;
+}
+
+int BARO_OVERSAMPLING_RATE = 8;
+
+bool BaroInit() {
+    if (!bmp.begin_I2C()) return false;
+    bmp.setOversampling(BARO_OVERSAMPLING_RATE);
+    return true;
+}
+
+int GPS_PORT = 1; // TODO
+int GPS_BAUD = 9600;
+
+HardwareSerial gpsSerial(GPS_PORT);
+
+bool GPSInit() {
+    gpsSerial.begin(GPS_BAUD);
+    return true;
 }
--- hardware.h
+++ hardware.h
@@ -4,6 +4,19 @@
 #include "FreeRTOS.h"
 #include "tasks/tasks.h"
 
+// Dependant on final hardware
+#include <ICM_20948.h>
+#include <Adafruit_BMP3XX.h>
+#include <HardwareSerial.h>
+#include <TinyGPS++.h>
+
 void HardwareInit(void);
+
+ICM_20948_I2C imu;
+Adafruit_BMP3XX bmp;
+TinyGPSPlus gps;
+
+HardwareSerial gpsSerial;
+
 
 #endif
