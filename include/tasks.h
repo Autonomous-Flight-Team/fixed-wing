64ef42225d0b2799bfaf73b6cc6efe1d27b021bd
Advik Sharma <70201060+advik-student-dev@users.noreply.github.com>
2026-02-02 17:28:01 -0800

    installed mavlink library in its c version into this repo. created receive tasks over 900 MHz and 2.4 GHz with their own separate queues to handle separate data streams

--- include/tasks.h
+++ include/tasks.h
@@ -9,14 +9,36 @@
 // #include "libraries/freertos-teensy/src/task.h"
 // #include "libraries/freertos-teensy/src/semphr.h"
 #include <arduino_freertos.h>
+#include <queue.h>
 #include <semphr.h>
 #include <task.h>
+
+extern "C" {
+#include "../mavlink/generated/include/mavlink/v2.0/common/mavlink.h"
+}
 
 extern SensorData_t sensorData;
 extern ControlOutput_t controlOutput;
 extern StateVector_t stateVector;
 extern SemaphoreHandle_t dataMutex;
 extern BlinkState_t blinkState;
+
+typedef enum {
+    LINK_900MHZ = 0,
+    LINK_24GHZ  = 1
+} MavlinkLink_t;
+
+typedef struct {
+    MavlinkLink_t link;
+    mavlink_message_t msg;
+} MavlinkRxPacket_t;
+
+extern QueueHandle_t mavlinkRxQueue900;
+extern QueueHandle_t mavlinkRxQueue24;
+extern volatile uint32_t mavlinkRxDrop900;
+extern volatile uint32_t mavlinkRxDrop24;
+extern volatile mavlink_message_t mavlinkLastTelemetry;
+extern volatile uint32_t mavlinkTelemetryCount;
 
 
 // Task Declarations
@@ -27,5 +49,10 @@ void PIDTask(void *pvParameters);
 void GSATxTask(void *pvParameters);
 void GSARxTask(void *pvParameters);
 void BlinkTask(void *pvParameters);
+void MavlinkRx900Task(void *pvParameters);
+void MavlinkRx24Task(void *pvParameters);
+void MavlinkControlDispatchTask(void *pvParameters);
+void MavlinkTelemetryDispatchTask(void *pvParameters);
+
 
 #endif
