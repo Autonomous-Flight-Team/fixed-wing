c887d524b5df49cd8a99a9bf0236dd3915a6dc65
Advik Sharma <70201060+advik-student-dev@users.noreply.github.com>
2026-04-27 16:15:55 -0700

    blink task fix

--- src/blink_task.cpp
+++ src/blink_task.cpp
@@ -6,11 +6,9 @@ void BlinkTask(void *pvParameters) {
   for (;;) {
     TickType_t lastWake = xTaskGetTickCount();
     const TickType_t freq = pdMS_TO_TICKS(MS_PER_TICK);
-    if (xSemaphoreTake(dataMutex, portMAX_DELAY)) {
-        digitalWrite(arduino::LED_BUILTIN, (blinkState.on) ? arduino::LOW : arduino::HIGH);
-        blinkState.on = !blinkState.on;
-        xSemaphoreGive(dataMutex);
-    }
+    digitalWrite(arduino::LED_BUILTIN, (blinkState.on) ? arduino::LOW : arduino::HIGH);
+    blinkState.on = !blinkState.on;
+    xSemaphoreGive(dataMutex);
     vTaskDelayUntil(&lastWake, freq);
   }
 }
---
+++ src/mavlink_quadcopter_packet_forward_task.cpp
@@ -0,0 +1,13 @@
+/*
+Advik Sharma - github.com/jpyces
+*/
+
+#include "tasks.h"
+
+#include <arduino_freertos.h>
+#include <Arduino.h>
+#include <queue.h>
+
+void quadcopterPacketForwardTask (void *pvParameters){
+    
+}
