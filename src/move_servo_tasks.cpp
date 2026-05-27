ba28021d1b2cc02061971108195bf341a6ecb8a0
lightningstar425 <lightningstar427@gmail.com>
2026-05-27 16:40:36 -0700

    updated servo mappings

--- src/move_servo_tasks.cpp
+++ src/move_servo_tasks.cpp
@@ -19,6 +19,11 @@ int map_throttle(int cont_throttle)
     return cont_throttle / 10;
 }
 
+float map_servos(int joy_stick_loc, int base)
+{
+    return (joy_stick_loc / 1000.0) * base + base;
+}
+
 // MIGHT NEED FOR SIM TO WORK
 // namespace
 // {
@@ -65,9 +70,9 @@ void set_state(mavlink_manual_control_t *controllerData, SetServoStates_t *servo
 
     servoStates->set_throttle = map_throttle(controllerData->z) + 1000; // passes 0 -1000 -> needs to be maped to 0 to 100
 
-    servoStates->set_elevator = clamp(servoStates->set_elevator + controllerData->x * elevator_rate, (float)0, elevator_limit);
-    servoStates->set_aileron = clamp(servoStates->set_aileron + controllerData->y * aileron_rate, (float)0, aileron_limit);
-    servoStates->set_rudder = clamp(servoStates->set_rudder + controllerData->r * rudder_rate, (float)0, rudder_limit);
+    servoStates->set_elevator = clamp(map_servos(controllerData->x, elevator_neutral), (float)0, elevator_limit);
+    servoStates->set_aileron = clamp(map_servos(controllerData->y, aileron_neutral), (float)0, aileron_limit);
+    servoStates->set_rudder = clamp(map_servos(controllerData->r, rudder_neutral), (float)0, rudder_limit);
 
     bool buttonState[16];
     for (int i = 0; i < 16; i++)
@@ -139,8 +144,7 @@ void set_state(mavlink_manual_control_t *controllerData, SetServoStates_t *servo
         clamp(servoStates->set_aileron + (aileronInput * aileron_rate), 0.0f, aileron_limit);
     servoStates->set_rudder =
         clamp(servoStates->set_rudder + (rudderInput * rudder_rate), 0.0f, rudder_limit);*/
-  
-  
+
     // Serial.print("Elevator: ");
     // Serial.println(servoStates->set_elevator);
     // Serial.print(" Rudder:");
